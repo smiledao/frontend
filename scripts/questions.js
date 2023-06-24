730ada310394b5ce6d972f513f65036c55167d00
rp753 <105857000+rp753@users.noreply.github.com>
2023-06-24 15:03:19 -0400

    separated some index.html content into index.js

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,1 +1,220 @@
 
+//Metamask sending trasactions:
+//https://docs.metamask.io/guide/sending-transactions.html#transaction-parameters
+
+detectMetamaskInstalled(); //When the page is opened check for error handling issues.
+
+let accounts = []; ////Empty array to be filled once Metamask is called.
+document.getElementById("enableEthereumButton").innerHTML =
+  "Connect Metamask 🦊";
+document.getElementById("getValueStateSmartContract").innerHTML = "Loading...";
+
+const sepoliaChainId = 11155111;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum); //Imported ethers from index.html with "<script src="https://cdn.ethers.io/lib/ethers-5.6.umd.min.js" type="text/javascript"></script>".
+
+// const signer = provider.getSigner(); //Do this when the user clicks "enableEthereumButton" which will call getAccount() to get the signer private key for the provider.
+
+const contractAddress_JS = "0xBBE97Afb978E19033e0BDa692E6034F5b3B91312";
+const contractABI_JS = [
+  { anonymous: false, inputs: [], name: "setEvent", type: "event" },
+  {
+    inputs: [{ internalType: "uint256", name: "x", type: "uint256" }],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "storedData",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const contractDefined_JS = new ethers.Contract(
+  contractAddress_JS,
+  contractABI_JS,
+  provider
+);
+
+// const contractDefined_JS = new ethers.Contract(contractAddress_JS, contractABI_JS, signer);
+
+getDataOnChainToLoad();
+
+async function getDataOnChainToLoad() {
+  let chainIdConnected = await getChainIdConnected();
+
+  if (chainIdConnected == sepoliaChainId) {
+    getStoredData();
+  }
+  if (chainIdConnected != sepoliaChainId) {
+    document.getElementById("getValueStateSmartContract").innerHTML =
+      "Install Metamask and select Sepolia Testnet to have a Web3 provider to read blockchain data.";
+  }
+}
+
+async function getStoredData() {
+  let storedDataCallValue = await contractDefined_JS.storedData();
+  if (storedDataCallValue === undefined) {
+    document.getElementById("getValueStateSmartContract").innerHTML =
+      "Install Metamask and select Sepolia Testnet to have a Web3 provider to read blockchain data.";
+  } else {
+    document.getElementById("getValueStateSmartContract").innerHTML =
+      storedDataCallValue;
+  }
+}
+
+async function sentTxAsync(x) {
+  const callDataObject = await contractDefined_JS.populateTransaction.set(x);
+  const txData = callDataObject.data;
+
+  ethereum
+    .request({
+      method: "eth_sendTransaction",
+      params: [
+        {
+          from: accounts[0],
+          to: contractAddress_JS,
+          data: txData,
+        },
+      ],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+}
+
+contractDefined_JS.on("setEvent", () => {
+  getStoredData();
+});
+
+//Connect to Metamask.
+const ethereumButton = document.querySelector("#enableEthereumButton");
+ethereumButton.addEventListener("click", () => {
+  detectMetamaskInstalled();
+  enableMetamaskOnSepolia();
+});
+
+// MODIFY CONTRACT STATE WITH SET FUNCTION WITH PREDEFINED DATA FROM WEB3.JS
+const changeStateInContractEvent = document.querySelector(
+  ".changeStateInContractEvent"
+);
+changeStateInContractEvent.addEventListener("click", () => {
+  checkAddressMissingMetamask();
+
+  var inputContractText = document
+    .getElementById("setValueSmartContract")
+    .value.toString();
+
+  if (/^\d+$/.test(inputContractText) == false) {
+    alert("Can only accept numeric characters.");
+    return;
+  }
+
+  if (BigInt(inputContractText) > BigInt(2 ** 256) - BigInt(1)) {
+    alert("Value is larger than uin256 max value ((2^256)-1).");
+    return;
+  }
+
+  sentTxAsync(inputContractText);
+});
+
+//If Metamask is not detected the user will be told to install Metamask.
+function detectMetamaskInstalled() {
+  try {
+    ethereum.isMetaMask;
+  } catch (missingMetamask) {
+    alert(
+      "Metamask not detected in browser! Install Metamask browser extension, then refresh page!"
+    );
+    document.getElementById("getValueStateSmartContract").innerHTML =
+      "Install Metamask and select Sepolia Testnet to have a Web3 provider to read blockchain data.";
+  }
+}
+
+//Alert user to connect their Metamask address to the site before doing any transactions.
+function checkAddressMissingMetamask() {
+  if (accounts.length == 0) {
+    alert(
+      "No address from Metamask found. Click the top button to connect your Metamask account then try again without refreshing the page."
+    );
+  }
+}
+
+async function getChainIdConnected() {
+  const connectedNetworkObject = await provider.getNetwork();
+  const chainIdConnected = connectedNetworkObject.chainId;
+  return chainIdConnected;
+}
+
+async function getAccount() {
+  accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  const signer = provider.getSigner();
+  document.getElementById("enableEthereumButton").innerText =
+    accounts[0].substr(0, 5) + "..." + accounts[0].substr(38, 4);
+}
+
+async function enableMetamaskOnSepolia() {
+  //Get account details from Metamask wallet.
+  getAccount();
+  //Check if user is on the Sepolia testnet. If not, alert them to change to Sepolia.
+  if (window.ethereum.networkVersion != sepoliaChainId) {
+    // alert("You are not on the Sepolia Testnet! Please switch to Sepolia and refresh page.")
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [
+          {
+            chainId: "0xaa36a7",
+          },
+        ],
+      });
+      location.reload();
+      // alert("Failed to add the network at chainId " + sepoliaChainId + " with wallet_addEthereumChain request. Add the network with https://chainlist.org/ or do it manually. Error log: " + error.message)
+    } catch (error) {
+      alert(
+        "Failed to add the network at chainId " +
+          sepoliaChainId +
+          " with wallet_addEthereumChain request. Add the network with https://chainlist.org/ or do it manually. Error log: " +
+          error.message
+      );
+    }
+  }
+}
+
+function loadImg() {
+  $("#frame").attr("src", URL.createObjectURL(event.target.files[0]));
+}
+
+async function startCamera() {
+  const videoElement = document.getElementById("video-preview");
+  let stream = null;
+
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    videoElement.srcObject = stream;
+    videoElement.play();
+  } catch (err) {
+    window.alert("Please allow camera access to use this feature.");
+  }
+}
+
+function takePhoto() {
+  const canvas = document.createElement("canvas");
+  canvas.display = "none";
+
+  const videoElement = document.getElementById("video-preview");
+
+  canvas.width = videoElement.videoWidth;
+  canvas.height = videoElement.videoHeight;
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(videoElement, 0, 0);
+
+  let image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log("Image taken: ", image);
+}
